e7e3411fda5a3512a6bebb18cc073c816e1992c2
Sean Worthington <sean@worthington.net>
2020-08-31 20:49:04 -0700

    Update action.php

--- sample/action.php
+++ sample/action.php
@@ -1,20 +1,15 @@
 <?php
         $mywallet = $_GET['merchant_skywallet'];
+	$customer_skywallet=$_GET['customer_skywallet'];
 	$amount_due = $_GET['amount'];
-	$guid = $_GET['guid'];
-      	$customer_skywallet=$_GET['customer_skywallet'];
+	$memo = $_GET['guid'];
 
-
-	$command = "/opt/raida_go view_receipt $guid $mywallet";
+	$command = "/opt/raida_go view_receipt $memo $mywallet";
 	echo "<br><b>The command is:</b> $command";
-	$json_obj = exec($command, $outarray, $error_code);
 
-	echo "<br><b>The response:</b> <code>$json_obj</code><br>";
+	$json_obj = exec($command, $outarray, $error_code);
+	echo "<br><b>The response from raida_go:</b> <code>$json_obj</code><br>";
 
 	$arr = json_decode($json_obj, true);
-
-	echo "<br><b>$customer_skywallet sent:</b>".intval($arr["amount_verified"]);
-
-
-
+	echo "<br><b>raida_go verified that $customer_skywallet sent $merchant_skywallet this amount:</b>".intval($arr["amount_verified"]);
 ?>
