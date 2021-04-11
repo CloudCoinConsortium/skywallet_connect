8c2a20471e6fabb4b5f4b067bb50597a140c5609
Sean Worthington <sean@worthington.net>
2021-04-10 23:44:23 -0700

    Changed to SKywallet Connect

--- sample/balance.php
+++ sample/balance.php
@@ -1,26 +1,26 @@
 <?php
 
 
-$raida_go = "/usr/bin/raida_go";
-if (!file_exists($raida_go))
-        die("Raida Go not found");
+$skywallet_connect = "/usr/bin/skywallet_connect";
+if (!file_exists($skywallet_connect))
+        die("skywallet_connect not found");
 
-if (!is_executable($raida_go))
-        die("Raida Go doesn't have exec permissions");
+if (!is_executable($skywallet_connect))
+        die("skywallet_connect doesn't have exec permissions");
 
 
 function getbalance() {
 	 
 	
-	global $raida_go;
+	global $skywallet_connect;
 	
 	$memo = base64_encode($meta); 
-	$cmd =  "$raida_go balance";
+	$cmd =  "$skywallet_connect balance";
 	
 	// Exec the binary
 	$json = exec($cmd, $outarray, $error_code);
 	if ($error_code != 0) {
-			echo "Invalid response from raida_go: $error_code, Output $json";
+			echo "Invalid response from skywallet_connect: $error_code, Output $json";
 			return 1;
 	}
 	
